fcdfb20eae7bd2b607f3192e9bf25124c1f4f773
Arthur <arthur91021@gmail.com>
2023-11-28 11:28:16 -0800

    selecting charity by id works now

--- src/server/api/charities.js
+++ src/server/api/charities.js
@@ -15,3 +15,24 @@ router.get('/', async (req, res, next) => {
     next();
   }
 });
+
+
+//displays charity by id!
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    //grabs the id from the charity database
+    const id = +req.params.id;
+
+    const charityById = await prisma.post.findUnique({ where: { id } });
+    if(!charityById) {
+      return next({
+        status: 404, 
+        message: `Look buddy, theres nothing here, its ok, you can move along. :)`
+      });
+    }
+    res.json(charityById)
+  } catch {
+    next();
+  }
+});
