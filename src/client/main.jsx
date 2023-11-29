d09e051f9ddf66c27e24141879f2283a07f8ab2b
Scott Cousins <scousins1024@yahoo.com>
2023-11-29 09:57:58 -0600

    Added homepage element to main and HH intro.

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from '../components/features/Homepage.jsx';
 
 const router = createBrowserRouter([
   {
@@ -13,7 +14,7 @@ const router = createBrowserRouter([
         path: '/',
         // this element needs to be updated with the path
         // when component is ready
-        element: <App />
+        element: <HomePage />
       }
     ]
   }
---
+++ src/components/features/Homepage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Welcome to Help Harbor</h1>
+      <p>
+        Welcome to Help Harbor, a beacon of compassion and collaboration in the
+        world of philanthropy. At Help Harbor, our mission is simple yet
+        profound: to bridge the gap between charitable organizations in need and
+        individuals eager to make a difference. We provide a unique platform
+        where charities can post their specific requests for assistance, whether
+        it's financial support, volunteer manpower, or resources. Our website
+        serves as a virtual harbor, where these heartfelt pleas dock, waiting to
+        catch the attention of compassionate souls ready to extend a helping
+        hand. Together, we believe in the power of community and collective
+        goodwill, transforming the act of giving into a collaborative voyage
+        toward a better, more caring world. Join us at Help Harbor, where every
+        click, share, and contribution creates ripples of positive change.
+      </p>
+    </>
+  );
+}
