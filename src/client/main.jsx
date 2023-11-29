b077ff6e1b28a2444b871028c17669e320e854e5
Arthur <arthur91021@gmail.com>
2023-11-29 09:05:26 -0800

    jk jk provided store to mainjsx

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import store from './store/index.js';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -21,6 +22,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
