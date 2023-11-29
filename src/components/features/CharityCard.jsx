43e50fd7d7d341e4e0690761ae83fa6651640b4e
Julia Hocken <ju.hocken@gmail.com>
2023-11-29 11:53:51 -0500

    add single charity card component

---
+++ src/components/features/CharityCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import "../../server/prisma/seed";
+// import charity slice
+
+export default function CharityCard({ charity }) {
+    // const miniBio = charity.description.slice(0, 175) + "...";
+    return (
+        <li className="charity-card">
+            <div className="charity-image">
+                <img src={charity.image} />
+            </div>
+            <section className="charity-info">
+                <h2>{charity.title}</h2>
+                <h3>{charity.email}</h3>
+                <p>This is where a bio will go </p>
+                {/* <p>{miniBio}</p>
+                <Link to={`/charities/${charities.id}`}>More Info</Link> */}
+                {/* this will link to the details page for each charity */}
+            </section>
+        </li>
+    );
+}
