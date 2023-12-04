71d721e41b53cbecad2fdb3670a462ea72ccd61a
Scott Cousins <scousins1024@yahoo.com>
2023-12-04 14:31:36 -0600

    schema db push

--- src/client/components/charities/CharityDetails.jsx
+++ src/client/components/charities/CharityDetails.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { useParams } from 'react-router';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   useGetCharityQuery,
   useDeleteCharityMutation,
