f68ce89d35e8dd7ce0a0829672d62c4b7e924611
Arthur <arthur91021@gmail.com>
2023-12-04 08:13:40 -0800

    index.js in auth folder should have all code needed

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -5,30 +5,33 @@ const { ServerError } = require('../errors');
 const router = require('express').Router();
 module.exports = router;
 
-/** THIS CODE UNDER IS FOR THE AUTH STUFF LATER, DO NOT TOUCH UNTILL AUTHORIZED TO DO SO */
 
-// // Attaches user to res.locals if token is valid
-// router.use(async (req, res, next) => {
-//     // Check for token
-//     const authHeader = req.headers.authorization;
-//     const token = authHeader?.split(" ")[1]; // "Bearer <token>"
-//     if (!authHeader || !token) {
-//       return next();
-//     }
 
-//     // Get user from token
-//     try {
-//       const { id } = jwt.verify(token);
-//       const user = await prisma.user.findUniqueOrThrow({ where: { id } });
-//       res.locals.user = user;
-//       next();
-//     } catch (err) {
-//       console.error(err);
-//       next(new ServerError(401, "Invalid token."));
-//     }
-//   });
+//  Attaches user to res.locals if token is valid
+router.use(async (req, res, next) => {
+    //  Check for token
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(" ")[1]; // "Bearer <token>"
+     if (!authHeader || !token) {
+      return next();
+     }
 
-/**THIS IS THE END OF THE AUTH CODE STUFF, YOU MAY RESUME. */
+     // Get user from token
+     try {
+       const { id } = jwt.verify(token);
+       const user = await prisma.user.findUniqueOrThrow({ where: { id } });
+      res.locals.user = user;
+       next();
+     } catch (err) {
+       console.error(err);
+       next(new ServerError(401, "Invalid token. Yingshi, its your fault!!!"));
+     }
+  });
+
+
 
 //this route will grab all the charities from the database
 router.use('/charities', require('./charities'));
+
+//this route is to grab the auth stuff
+router.use('/auth', require("./auth"))
