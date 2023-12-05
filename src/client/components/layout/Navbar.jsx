bdd52dbf5790b914e4a1d55ccaf589a02124ea63
nikiosw <nikiosmwallace@gmail.com>
2023-12-05 18:50:51 -0500

    nav bar is complete, url links need to be updated, no responsive

--- src/client/components/layout/Navbar.jsx
+++ src/client/components/layout/Navbar.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Navbar.css';
-import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo2.png';
 import { MenuData } from './MenuData';
 
