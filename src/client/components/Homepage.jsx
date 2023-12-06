63f72bfd5406ac68d4870b1603219e94667b61bc
Scott Cousins <scousins1024@yahoo.com>
2023-12-06 10:49:31 -0600

    Login with auth working

--- src/client/components/Homepage.jsx
+++ src/client/components/Homepage.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import hanger from '../assets/hanger.png';
-import burgericon from '../assets/burgericon.png';
-import couch from '../assets/couch.png';
-import infinity from '../assets/infinity.png';
-import './Homepage.css'
+// import { Link } from 'react-router-dom';
+// import hanger from '../assets/hanger.png';
+// import burgericon from '../assets/burgericon.png';
+// import couch from '../assets/couch.png';
+// import infinity from '../assets/infinity.png';
+import './Homepage.css';
 
 export default function HomePage() {
   return (
     <>
-      <div class="background">
+      <div class='background'>
         <section>
-        <div class="welcomeMsg">
-      <h1>Welcome to Help Harbor</h1>
-      <p>
-        Welcome to Help Harbor, a beacon of compassion and collaboration in the
-        world of philanthropy. At Help Harbor, our mission is simple yet
-        profound: to bridge the gap between charitable organizations in need and
-        individuals eager to make a difference. We provide a unique platform
-        where charities can post their specific requests for assistance, whether
-        it's financial support, volunteer manpower, or resources. Our website
-        serves as a virtual harbor, where these heartfelt pleas dock, waiting to
-        catch the attention of compassionate souls ready to extend a helping
-        hand. Together, we believe in the power of community and collective
-        goodwill, transforming the act of giving into a collaborative voyage
-        toward a better, more caring world. Join us at Help Harbor, where every
-        click, share, and contribution creates ripples of positive change.
-      </p>
-        </div>
+          <div class='welcomeMsg'>
+            <h1>Welcome to Help Harbor</h1>
+            <p>
+              Welcome to Help Harbor, a beacon of compassion and collaboration
+              in the world of philanthropy. At Help Harbor, our mission is
+              simple yet profound: to bridge the gap between charitable
+              organizations in need and individuals eager to make a difference.
+              We provide a unique platform where charities can post their
+              specific requests for assistance, whether it's financial support,
+              volunteer manpower, or resources. Our website serves as a virtual
+              harbor, where these heartfelt pleas dock, waiting to catch the
+              attention of compassionate souls ready to extend a helping hand.
+              Together, we believe in the power of community and collective
+              goodwill, transforming the act of giving into a collaborative
+              voyage toward a better, more caring world. Join us at Help Harbor,
+              where every click, share, and contribution creates ripples of
+              positive change.
+            </p>
+          </div>
         </section>
       </div>
       {/* <Link to={'/clothes'}>
--- src/client/components/login/LoginForm.jsx
+++ src/client/components/login/LoginForm.jsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { React, useState } from 'react';
+import { useLoginMutation, useRegisterMutation } from './authslice';
 import '../login/Global.css';
+
 export default function LoginForm() {
-  const Navigate = useNavigate();
-  //Set state for username and passwordd
+  const navigate = useNavigate();
+
+  //Set state for username and password
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isLogin, setIsLogin] = useState(true);
+
   const authAction = isLogin ? 'Login' : 'Register';
+
   const altCopy = isLogin
     ? "Don't have an Account? Register here!"
     : 'Already have an account? Login';
+
   const [login, { error: loginError }] = useLoginMutation();
   const [register, { error: registerError }] = useRegisterMutation();
   const [loading, setLoading] = useState(false);
@@ -26,38 +33,51 @@ export default function LoginForm() {
     try {
       //we need to unwrap to handle and catch errors
       await authMethod(credentials).unwrap();
-      Navigate('/users/me');
+      navigate('/users/me');
     } catch (error) {
       setError(error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Login Form</h3>
-      <label>
-        Username:
-        <input
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
+    <>
+      <h1>{authAction}</h1>
+      <form onSubmit={handleSubmit}>
+        <h3>Login Form</h3>
+        <label>
+          Username:
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            autoComplete='username'
+          />
+        </label>
 
-      <label>
-        Password:
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button type='submit'>Login</button>
-      <p>
-        No Account? <Link to='/register'>Register here</Link>
-      </p>
-      <br />
-    </form>
+        <label>
+          Password:
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete='current-password'
+          />
+        </label>
+
+        <br />
+        <button>{authAction}</button>
+        <p>
+          No Account? <Link to='/register'>Register here</Link>
+        </p>
+        <br />
+      </form>
+
+      <a onClick={() => setIsLogin(!isLogin)}>{altCopy}</a>
+
+      {isLogin && loginError && <p role='alert'>{loginError}</p>}
+      {!isLogin && registerError && <p role='alert'>{registerError}</p>}
+      {loading && <p>Page is Loading...</p>}
+      {error && <p>Oops...Something went Wrong!</p>}
+    </>
   );
 }
