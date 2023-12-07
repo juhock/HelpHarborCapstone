2ea17fe89c23f5bab1fbabd0db6db01be6206035
Scott Cousins <scousins1024@yahoo.com>
2023-12-07 09:55:07 -0600

    accountSlice working with auth

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,37 +1,32 @@
 const { ServerError } = require('../errors');
 // const prisma = require("../prisma")
-// const jwt require("./auth/jwt") this is for the auth stuff later
+const jwt = require('./auth/jwt');
 
 const router = require('express').Router();
 module.exports = router;
 
-
-
 //  Attaches user to res.locals if token is valid
 router.use(async (req, res, next) => {
-    //  Check for token
-    const authHeader = req.headers.authorization;
-    const token = authHeader?.split(" ")[1]; // "Bearer <token>"
-     if (!authHeader || !token) {
-      return next();
-     }
+  //  Check for token
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(' ')[1]; // "Bearer <token>"
+  if (!authHeader || !token) {
+    return next();
+  }
 
-     // Get user from token
-     try {
-       const { id } = jwt.verify(token);
-       const user = await prisma.user.findUniqueOrThrow({ where: { id } });
-      res.locals.user = user;
-       next();
-     } catch (err) {
-       console.error(err);
-       next(new ServerError(401, "Invalid token. Yingshi, its your fault!!!"));
-     }
-  });
-
-
+  // Get user from token
+  try {
+    const { id } = jwt.verify(token);
+    const user = await prisma.user.findUniqueOrThrow({ where: { id } });
+    res.locals.user = user;
+    next();
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 //this route will grab all the charities from the database
 router.use('/charities', require('./charities'));
 
 //this route is to grab the auth stuff
-router.use('/auth', require("./auth"))
+router.use('/auth', require('./auth'));
