dd760ffdfe99cf11059a5bdb7a530c74b8f01ae3
Julia Hocken <ju.hocken@gmail.com>
2023-12-14 11:36:41 -0500

    adjusted new charity jsx

--- src/client/components/charities/NewCharity.jsx
+++ src/client/components/charities/NewCharity.jsx
@@ -1,26 +1,21 @@
-import { useState, useEffect } from 'react';
-import { useCreateCharityMutation } from './charitiesSlice';
-import { useParams, useNavigate } from 'react-router';
-
-
+import { useState, useEffect } from "react";
+import { useCreateCharityMutation } from "./charitiesSlice";
+import { useParams, useNavigate } from "react-router";
 
 /**Form for creating a new charity */
 export default function NewCharity() {
   const { id } = useParams();
-  const {data: charity, isLoading } = useCreateCharityMutation(id)
+  const { data: charity, isLoading } = useCreateCharityMutation(id);
   const navigate = useNavigate();
 
-
-
-
   const [createCharity] = useCreateCharityMutation();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [address, setAddress] = useState('');
-  const [category, setCategory] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [category, setCategory] = useState("");
 
   useEffect(() => {
     if (charity) {
@@ -34,24 +29,21 @@ export default function NewCharity() {
     }
   }, [charity]);
 
-
-
-
   /** Function to trigger  */
   const onCreate = async (e) => {
     e.preventDefault();
-   
-    createCharity({ 
-      title, 
-      description, 
-      image, 
-      email, 
-      phone, 
-      address, 
-      category 
+
+    createCharity({
+      title,
+      description,
+      image,
+      email,
+      phone,
+      address,
+      category,
     });
 
-    navigate('/charities')
+    navigate("/charities");
   };
 
   const handleCategoryChange = (evt) => {
@@ -61,49 +53,49 @@ export default function NewCharity() {
 
   return (
     <section>
-      <div className='global'>
+      <div className="global">
         <p>The Form to create new post</p>
         <form onSubmit={onCreate}>
           <input
-            type='text'
-            placeholder='Title'
+            type="text"
+            placeholder="Title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           ></input>
           <input
-            type='text'
-            placeholder='Description'
+            type="text"
+            placeholder="Description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></input>
           <input
-            type='text'
-            placeholder='Image'
+            type="text"
+            placeholder="Image"
             value={image}
             onChange={(e) => setImage(e.target.value)}
           ></input>
           <input
-            type='text'
-            placeholder='Email'
+            type="text"
+            placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></input>
           <input
-            type='text'
-            placeholder='Phone'
+            type="text"
+            placeholder="Phone"
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
           ></input>
           <input
-            type='text'
-            placeholder='Address'
+            type="text"
+            placeholder="Address"
             value={address}
             onChange={(e) => setAddress(e.target.value)}
           ></input>
           <select
-          name='category'
-          value={category}
-          onChange={(e) => handleCategoryChange(e)}
+            name="category"
+            value={category}
+            onChange={(e) => handleCategoryChange(e)}
           >
             <option>Please Select A Category</option>
             <option>Food</option>
