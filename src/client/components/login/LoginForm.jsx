1bf6884096263e8ac0b7ef14353faed1fc32ca6f
Julia Hocken <ju.hocken@gmail.com>
2023-12-15 11:45:53 -0500

    specific class names given to login form + css

--- src/client/components/login/LoginForm.jsx
+++ src/client/components/login/LoginForm.jsx
@@ -1,18 +1,18 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { React, useState } from 'react';
-import { useLoginMutation, useRegisterMutation } from './authslice';
-import './LoginForm.css';
+import { Link, useNavigate } from "react-router-dom";
+import { React, useState } from "react";
+import { useLoginMutation, useRegisterMutation } from "./authslice";
+import "./LoginForm.css";
 
 export default function LoginForm() {
   const navigate = useNavigate();
 
   //Set state for username and password
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(true);
 
-  const authAction = isLogin ? 'Login' : 'Register';
+  const authAction = isLogin ? "Login" : "Register";
 
   const [login, { error: loginError }] = useLoginMutation();
   const [register, { error: registerError }] = useRegisterMutation();
@@ -29,7 +29,7 @@ export default function LoginForm() {
     try {
       //we need to unwrap to handle and catch errors
       await authMethod(credentials).unwrap();
-      navigate('/users/me');
+      navigate("/users/me");
     } catch (error) {
       setError(error);
     }
@@ -39,48 +39,48 @@ export default function LoginForm() {
     <section>
       <div className="body">
         <div className="Login">
-      <div className="border">
-        <h1>{authAction}</h1>
-      </div>  
-        <form onSubmit={handleSubmit}>
-           
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              autoComplete="username"
-            />
-          </label>
-
-            <label>
-              Password:
+          <div className="border">
+            <h1>{authAction}</h1>
+          </div>
+          <form onSubmit={handleSubmit} id="loginForm">
+            <label className="labels">
+              Username:
               <input
-                type='password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                autoComplete='current-password'
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                autoComplete="username"
+                className="loginInput"
               />
             </label>
 
-          <br />
-          <button>{authAction}</button>
-          <p id="registerlink">
-            No Account? <Link to="/register">Register here</Link>
-          </p>
-          <br />
-        </form>
+            <label className="labels">
+              Password:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+                className="loginInput"
+              />
+            </label>
 
-        {isLogin && loginError && (
-          <p>Username or Password is Incorrect. Please try again.</p>
-        )}
-        {!isLogin && registerError && (
-          <p>Username or Password is Incorrect. Please try again.</p>
-        )}
-        
+            <br />
+            <button id="regButLogin">{authAction}</button>
+            <p id="registerlink">
+              No Account? <Link to="/register">Register here</Link>
+            </p>
+            <br />
+          </form>
+
+          {isLogin && loginError && (
+            <p>Username or Password is Incorrect. Please try again.</p>
+          )}
+          {!isLogin && registerError && (
+            <p>Username or Password is Incorrect. Please try again.</p>
+          )}
         </div>
-        </div>
+      </div>
     </section>
   );
 }
