acb72d285b1d75d58f61de67db5457711dd947e2
Julia Hocken <ju.hocken@gmail.com>
2023-12-16 11:36:11 -0500

    adjusted about us css

--- src/client/components/AboutUs.jsx
+++ src/client/components/AboutUs.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../components/layout/AboutUs.css';
+import React from "react";
+import "../components/layout/AboutUs.css";
 
 export default function AboutUs() {
   return (
-    <div className='bdy'>
-      <div className='about'>
-        <div className='inner-section'>
-          <h1>Mission Statement</h1>
-          <p className='text'>
+    <div className="bdy">
+      <div className="about">
+        <div className="inner-section">
+          <h1 className="headerAbout">Mission Statement</h1>
+          <p className="text">
             Welcome to Help Harbor, a beacon of compassion and collaboration in
             the world of philanthropy. At Help Harbor, our mission is simple yet
             profound: to bridge the gap between charitable organizations in need
@@ -22,7 +22,20 @@ export default function AboutUs() {
             better, more caring world. Join us at Help Harbor, where every
             click, share, and contribution creates ripples of positive change.
           </p>
-          <div class='skills'>
+          <h1 className="headerAbout" id="ourTeam">
+            Our Team
+          </h1>
+          <p className="text">
+            We're a tight-knit crew of junior software developers fresh out of
+            Full Stack Academy's rigorous Software Developer immersive bootcamp.
+            Our squad is made up of passionate individuals who collaborated
+            seamlessly to craft a solution poised to make a positive impact on
+            the world. This website marks the beginning of our journey, and
+            we're convinced that our collective dedication will lead to the
+            creation of applications that can quickly resonate across the globe,
+            impacting diverse cultures beyond our own.
+          </p>
+          <div class="skills">
             <button>Contact Us</button>
           </div>
         </div>
